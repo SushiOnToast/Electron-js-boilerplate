40df048748150addddfc17164fb607ab9d31f7e2
ACC-ASS-2024-00030 <webappick-acc-ass-2024-00030@SHOJOL-ISLAM.local>
2025-05-12 16:08:52 +0600

    init

---
+++ electron/main.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-undef */
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  // for devtools
+  // win.webContents.openDevTools();
+
+  const VITE_DEV_SERVER_URL = process.env.VITE_DEV_SERVER_URL;
+  if (VITE_DEV_SERVER_URL) {
+    win.loadURL(VITE_DEV_SERVER_URL);
+  } else {
+    win.loadFile(path.join(__dirname, "../dist/index.html"));
+  }
+};
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+function About() {
+  return (
+    <div>
+      <h1 className="text-center text-2xl font-bold my-5">About Page</h1>
+      <p className="text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolorem unde beatae odio cupiditate temporibus
+        numquam adipisci voluptatum tempora cumque fugit sunt expedita quae blanditiis saepe alias, natus consequatur
+        necessitatibus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolorem unde beatae odio
+        cupiditate temporibus numquam adipisci voluptatum tempora cumque fugit sunt expedita quae blanditiis saepe
+        alias, natus consequatur necessitatibus?
+      </p>
+    </div>
+  );
+}
+
+export default About;
